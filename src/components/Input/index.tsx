7509b839f3314740673c892ee85bad706506196d
amaralc <amaral.calil@gmail.com>
2020-12-15 13:06:03 -0300

    FEATURE / Cria referencias para os campos e utiliza useEffect e registerField

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,5 +1,6 @@
-import React, { InputHTMLAttributes } from 'react';
+import React, { InputHTMLAttributes, useEffect, useRef } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
@@ -12,11 +13,22 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 }
 
 /** Define input e converte propriedade icon em Icon para react entender como componente */
-const Input: React.FC<InputProps> = ({ icon: Icon, ...rest }) => {
+const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
+  const inputRef = useRef(null);
+  const { fieldName, defaultValue, error, registerField } = useField(name);
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      ref: inputRef.current,
+      path: 'value',
+    });
+  }, [fieldName, registerField]);
+
   return (
     <Container>
       {Icon && <Icon size={20} />}
-      <input {...rest} type="text" />
+      <input defaultValue={defaultValue} ref={inputRef} {...rest} type="text" />
     </Container>
   );
 };
