f24791c1121c337d7314eb7418f5188c46f64a6f
amaralc <amaral.calil@gmail.com>
2020-12-15 11:42:13 -0300

    DOC / Adiciona comentarios

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,6 +2,7 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
+/** Define tipo (interface vazia ou baseada em outro tipo) do botao */
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<ButtonProps> = ({ children, ...rest }) => {
