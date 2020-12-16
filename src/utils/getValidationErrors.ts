e91057c0ca2956a61f02ebba5c31052ee7ace7c6
amaralc <amaral.calil@gmail.com>
2020-12-15 22:43:44 -0300

    FEATURE / Define erros buscando dos erros de validacao do yup

---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,16 @@
+import { ValidationError } from 'yup';
+
+interface Errors {
+  /** Define tipo com chave e valor (objeto) */
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    validationErrors[error.path] = error.message;
+  });
+
+  return validationErrors;
+}
