2d4c4e7f1978c423ba071b36c6027cf091670752
amaralc <amaral.calil@gmail.com>
2020-12-17 20:00:27 -0300

    FEATURE / Cria interface User

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -2,9 +2,15 @@ import React, { createContext, useCallback, useState, useContext } from 'react';
 
 import api from '../services/api';
 
+interface User {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
+
 interface AuthState {
   token: string;
-  user: Record<string, unknown>;
+  user: User;
 }
 
 interface SignInCredentials {
@@ -14,7 +20,7 @@ interface SignInCredentials {
 
 /** Formato dos dados do contexto */
 interface AuthContextData {
-  user: Record<string, unknown>;
+  user: User;
   signIn(credentials: SignInCredentials): Promise<void>;
   signOut(): void;
 }
